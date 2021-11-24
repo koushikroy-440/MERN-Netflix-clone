dab21b9952482ee761c49cfeae4dbad4df1df893
koushikroy-440 <koushikroy295@gmail.com>
2021-11-24 15:50:10 +0530

    end clients

--- netflix/src/components/ListItem/Listitem.jsx
+++ netflix/src/components/ListItem/Listitem.jsx
@@ -1,43 +1,100 @@
 import './Listitem.scss';
 import {
-    PlayArrow,
-    Add,
-    ThumbUpAltOutlined,
-    ThumbDownOutlined,
-  } from "@material-ui/icons";
-  import { useState,useEffect } from "react";
-  import axios from 'axios';
+  PlayArrow,
+  Add,
+  ThumbUpAltOutlined,
+  ThumbDownOutlined,
+} from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Watch from '../../pages/watch/Watch';
 export const Listitem = ({ index,item }) => {
+    // const [isHovered, setIsHovered] = useState(false);
+    // const [movie,setMovie] = useState({});
+    // useEffect(() => {
+    //   const getMovie = async() => {
+    //     try{
+    //       const res = await axios.get('/movies/find/'+item,{
+    //         headers: {
+    //           token:
+    //           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOGJjZGFhMjk0ZWVhNzhiNDg1YWI1NiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzY0NjY2NywiZXhwIjoxNjM4MDc4NjY3fQ._7KGPlv11TV_MMR_FDa14YQy7wtM74-YUPmBTeAFuDI",
+    //         },
+    //       });
+    //       setMovie(res.data);
+    //     } catch(err){
+    //       console.log(err);
+    //     }
+    //   };
+    //   getMovie();
+    // }, [item]);
+    // return (
+    //   <Link to={{pathname:"/watch", movie:movie,akash:"akasj"}}>
+    //   <div
+    //     className="listItem"
+    //     style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+    //     onMouseEnter={() => setIsHovered(true)}
+    //     onMouseLeave={() => setIsHovered(false)}
+    //   >
+    //     <img
+    //       src={movie.img}
+    //       alt=""
+    //     />
+    //     {isHovered && (
+    //       <>
+    //         <video src={movie.trailer} autoPlay={true} loop />
+    //         <div className="itemInfo">
+    //           <div className="icons">
+    //             <PlayArrow className="icon" />
+    //             <Add className="icon" />
+    //             <ThumbUpAltOutlined className="icon" />
+    //             <ThumbDownOutlined className="icon" />
+    //           </div>
+    //           <div className="itemInfoTop">
+    //             <span>{movie.duration}</span>
+    //             <span className="limit">+{movie.limit}</span>
+    //             <span>{movie.year}</span>
+    //           </div>
+    //           <div className="desc">
+    //             {movie.desc}
+    //           </div>
+    //           <div className="genre">{movie.genre}</div>
+    //         </div>
+    //       </>
+    //     )}
+    //   </div>
+    //   </Link>
+    // );
     const [isHovered, setIsHovered] = useState(false);
-    const [movie,setMovie] = useState({});
-    
-    useEffect(() => {
-      const getMovie = async() => {
-        try{
-          const res = await axios.get('/movies/find/'+item,{
-            headers: {
-              token:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOGJjZGFhMjk0ZWVhNzhiNDg1YWI1NiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzY0NjY2NywiZXhwIjoxNjM4MDc4NjY3fQ._7KGPlv11TV_MMR_FDa14YQy7wtM74-YUPmBTeAFuDI",
-            },
-          });
-          setMovie(res.data);
-        } catch(err){
-          console.log(err);
-        }
-      };
-      getMovie();
-    }, [item]);
-    return (
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    const getMovie = async () => {
+      try {
+        const res = await axios.get("/movies/find/" + item, {
+          headers: {
+                      token:
+                      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOGJjZGFhMjk0ZWVhNzhiNDg1YWI1NiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzY0NjY2NywiZXhwIjoxNjM4MDc4NjY3fQ._7KGPlv11TV_MMR_FDa14YQy7wtM74-YUPmBTeAFuDI",
+                    },
+        });
+        setMovie(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMovie();
+  }, [item]);
+
+  return (
+    <Link to={{ pathname: "/watch",movie:movie }}>
+
       <div
         className="listItem"
         style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
-        <img
-          src={movie.img}
-          alt=""
-        />
+        <img src={movie?.imgSm} alt="" />
         {isHovered && (
           <>
             <video src={movie.trailer} autoPlay={true} loop />
@@ -53,13 +110,12 @@ export const Listitem = ({ index,item }) => {
                 <span className="limit">+{movie.limit}</span>
                 <span>{movie.year}</span>
               </div>
-              <div className="desc">
-                {movie.desc}
-              </div>
+              <div className="desc">{movie.desc}</div>
               <div className="genre">{movie.genre}</div>
             </div>
           </>
         )}
       </div>
-    );
+    </Link>
+  );
 }
--- netflix/src/components/featured/featured.jsx
+++ netflix/src/components/featured/featured.jsx
@@ -1,8 +1,28 @@
 
 import { InfoOutlined, PlayArrow } from "@material-ui/icons";
 import "./featured.scss";
+import { useState,useEffect } from 'react';
+import axios from 'axios';
 
 export default function Featured({ type }) {
+  const [content,setContent] = useState({});
+  useEffect(()=>{
+    const getRandomContent = async ()=>{
+      try{
+        const res = await axios.get(`/movies/random?type=${type}`,{
+          headers: {
+            token:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOGJjZGFhMjk0ZWVhNzhiNDg1YWI1NiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzY0NjY2NywiZXhwIjoxNjM4MDc4NjY3fQ._7KGPlv11TV_MMR_FDa14YQy7wtM74-YUPmBTeAFuDI",
+          }
+        });
+        
+        setContent(res.data[0]);
+      }catch(err){
+        console.log(err);
+      }
+    };
+    getRandomContent();
+  }, [type]);
   return (
     <div className="featured">
       {type && (
@@ -27,19 +47,16 @@ export default function Featured({ type }) {
         </div>
       )}
       <img
-        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        src={content.img}
         alt=""
       />
       <div className="info">
         <img
-          src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
+          src={content.imgTitle}
           alt=""
         />
         <span className="desc">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-          adipisci repellendus eum quasi illo, velit numquam, maxime tempora
-          sint deleniti, aliquid qui? Facilis, adipisci! Ratione hic repudiandae
-          temporibus eum earum?
+         {content.desc}
         </span>
         <div className="buttons">
           <button className="play">
